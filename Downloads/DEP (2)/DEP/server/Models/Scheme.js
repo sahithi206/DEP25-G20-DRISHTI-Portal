870b481692708f25f6789bc794356287dd1283e1
GVarsha23 <146741973+GVarsha23@users.noreply.github.com>
2025-03-11 12:50:47 +0530

    Delete Downloads directory

--- Downloads/DEP (2)/DEP/server/Models/Scheme.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const SchemeSchema = new mongoose.Schema({
-  name: String,
-  description: String
-});
-
-module.exports = mongoose.model("Scheme", SchemeSchema);
