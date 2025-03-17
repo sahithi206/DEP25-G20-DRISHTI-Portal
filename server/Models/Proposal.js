5832107ce025440d9c22464a7b86202f40251991
shalini palla <144434562+shalinipalla005@users.noreply.github.com>
2025-03-18 01:31:04 +0530

    Update Proposal.js

--- server/Models/Proposal.js
+++ server/Models/Proposal.js
@@ -3,8 +3,16 @@ const Schema = mongoose.Schema;
 
 const proposalSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
-    status: { type: String, enum: ["Unsaved","Pending","Sanctioned", "Approved", "Rejected"], required: true },
+    status: { type: String, enum: ["Unsaved","Pending", "Approved", "Rejected"], required: true },
+    comments: [
+        {
+          text: { type: String, required: true },
+          createdAt: { type: Date, default: Date.now },
+        }
+      ],
     Scheme:{type:String}
 });
+
+
 
 module.exports = mongoose.model("Proposal", proposalSchema);
