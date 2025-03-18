9652b2856a1fffb1b9250b05e5f598e904f86d54
sahithi206 <2022eeb1218@iitrpr.ac.in>
2025-03-18 16:49:03 +0530

    push

--- server/Models/Proposal.js
+++ server/Models/Proposal.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const proposalSchema = new Schema({
     userId: { type: Schema.Types.ObjectId, ref: "users", required: true },
-    status: { type: String, enum: ["Unsaved","Pending", "Approved", "Rejected"], required: true },
+    status: { type: String, enum: ["Unsaved","Sanctioned","Pending", "Approved", "Rejected"], required: true },
     comments: [
         {
           text: { type: String, required: true },
