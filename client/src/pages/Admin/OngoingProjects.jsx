b29e6106058d85d34fa71ab53d8742fb03a9a7b3
Varsha <2022csb1082@iitrpr.ac.in>
2025-04-17 01:15:03 +0530

    admin portal changes

--- client/src/pages/Admin/OngoingProjects.jsx
+++ client/src/pages/Admin/OngoingProjects.jsx
@@ -28,62 +28,62 @@ const AdminProposalReview = () => {
             console.log("Bank Details:", selectedProposal.bankInfo);
         }
     }, [selectedProposal]);
-   const [schemes,setSchemes]=useState([]);
-       useEffect(()=>{
-           const getSchemes= async()=>{
-               try{
-                   const res = await fetch(`${URL}schemes/get-allschemes`,{
-                       headers:{
-                           "Content-Type":"application/json",
-                           accessToken:localStorage.getItem("token")
-                       }
-                   })
-                   const data= await res.json();
-                   console.log(data);
-                   setSchemes(data);
-               }catch(e){
-                   console.log(e);
-               }
-           }
-           getSchemes();
-          }, [URL])
-       
-       const [schemeFilter, setFilter] = useState(""); 
-       const [sortOrder, setSortOrder] = useState("desc"); 
-         const [searchTitle, setSearchTitle] = useState("");
-                 const [filteredProjects, setFilteredUc] = useState([]);
-       useEffect(() => {
-           const filteredProjects = async () => {
-               let filtered = proposals;
-   
-               if (searchTitle) {
-                   const searchTerm = searchTitle.toLowerCase();
-                   filtered = filtered.filter((project) => {
-                       if (project?.generalInfo?.instituteName?.toLowerCase().includes(searchTerm)) return true;
-                       if (project?.piInfo?.piList?.some(member => member.name.toLowerCase().includes(searchTerm))) return true;
-                       if (project?.project?.Scheme?.name.toLowerCase().includes(searchTerm)) return true;
-   
-                       return false;
-                   });
-               }
-   
-               if (schemeFilter) {
-                   filtered = filtered.filter(project => project.project.Scheme.name === schemeFilter);
-               }
-   
-               if (sortOrder) {
-                   filtered = filtered.sort((a, b) => {
-                       const dateA = new Date(a.project.date);
-                       const dateB = new Date(b.project.date);
-                       return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
-                   });
-               }
-   
-               setFilteredUc(filtered);
-           };
-   
-           filteredProjects();
-       }, [searchTitle, sortOrder, proposals, schemeFilter]);
+    const [schemes, setSchemes] = useState([]);
+    useEffect(() => {
+        const getSchemes = async () => {
+            try {
+                const res = await fetch(`${URL}schemes/get-allschemes`, {
+                    headers: {
+                        "Content-Type": "application/json",
+                        accessToken: localStorage.getItem("token")
+                    }
+                })
+                const data = await res.json();
+                console.log(data);
+                setSchemes(data);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+        getSchemes();
+    }, [URL])
+
+    const [schemeFilter, setFilter] = useState("");
+    const [sortOrder, setSortOrder] = useState("desc");
+    const [searchTitle, setSearchTitle] = useState("");
+    const [filteredProjects, setFilteredUc] = useState([]);
+    useEffect(() => {
+        const filteredProjects = async () => {
+            let filtered = proposals;
+
+            if (searchTitle) {
+                const searchTerm = searchTitle.toLowerCase();
+                filtered = filtered.filter((project) => {
+                    if (project?.generalInfo?.instituteName?.toLowerCase().includes(searchTerm)) return true;
+                    if (project?.piInfo?.piList?.some(member => member.name.toLowerCase().includes(searchTerm))) return true;
+                    if (project?.project?.Scheme?.name.toLowerCase().includes(searchTerm)) return true;
+
+                    return false;
+                });
+            }
+
+            if (schemeFilter) {
+                filtered = filtered.filter(project => project.project.Scheme.name === schemeFilter);
+            }
+
+            if (sortOrder) {
+                filtered = filtered.sort((a, b) => {
+                    const dateA = new Date(a.project.date);
+                    const dateB = new Date(b.project.date);
+                    return sortOrder === "asc" ? dateA - dateB : dateB - dateA;
+                });
+            }
+
+            setFilteredUc(filtered);
+        };
+
+        filteredProjects();
+    }, [searchTitle, sortOrder, proposals, schemeFilter]);
     useEffect(() => {
         const fetchPendingProposals = async () => {
             const token = localStorage.getItem("token");
@@ -98,7 +98,7 @@ const AdminProposalReview = () => {
                     headers: { "accessToken": token },
                 });
                 const data = await response.json();
-                setProposals(data.data||[]);
+                setProposals(data.data || []);
 
                 console.log(data);
             } catch (err) {
@@ -110,7 +110,7 @@ const AdminProposalReview = () => {
         fetchPendingProposals();
     }, []);
 
-   
+
 
     return (
         <div className="flex h-screen bg-gray-100">
@@ -129,59 +129,55 @@ const AdminProposalReview = () => {
                     </div>
                 )}
 
-                
-
-                  
-
                 <div className="mt-6 bg-white p-6 rounded-lg shadow-md">
-                <div className="flex space-x-4  mb-4">
-                   <div className="relative flex-grow">
-                       <input
-                           type="text"
-                           placeholder="Search projects by PI name ..."
-                           value={searchTitle}
-                           onChange={(e) => setSearchTitle(e.target.value)}
-                           className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                       />
-                       <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
-                           <svg
-                               xmlns="http://www.w3.org/2000/svg"
-                               className="h-5 w-5"
-                               fill="none"
-                               viewBox="0 0 24 24"
-                               stroke="currentColor"
-                               role="img"
-                               aria-label="Search icon"
-                           >
-                               <path
-                                   strokeLinecap="round"
-                                   strokeLinejoin="round"
-                                   strokeWidth={2}
-                                   d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-                               />
-                           </svg>
-                       </div>
-                   </div>
-                   <select
-                       value={sortOrder}
-                       onChange={(e) => setSortOrder(e.target.value)}
-                       className="w-40 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                   >
-                       <option value="newest">Newest</option>
-                       <option value="oldest">Oldest</option>
-                   </select>
-                   <select
-                       value={schemeFilter}
-                       onChange={(e) => setFilter(e.target.value)}
-                       className="w-40 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
-                   >   
-                       <option value="">All</option>
-                       {schemes && schemes.length > 0 && schemes.map((val, index) => (
-                           <option value={val.name} key={val._id}>{val.name}</option>
-                       ))}
+                    <div className="flex space-x-4  mb-4">
+                        <div className="relative flex-grow">
+                            <input
+                                type="text"
+                                placeholder="Search projects by PI name ..."
+                                value={searchTitle}
+                                onChange={(e) => setSearchTitle(e.target.value)}
+                                className="w-full pl-10 pr-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                            />
+                            <div className="absolute left-3 top-1/2 transform -translate-y-1/2 text-gray-400">
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    className="h-5 w-5"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    stroke="currentColor"
+                                    role="img"
+                                    aria-label="Search icon"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        strokeWidth={2}
+                                        d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                        <select
+                            value={sortOrder}
+                            onChange={(e) => setSortOrder(e.target.value)}
+                            className="w-40 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        >
+                            <option value="newest">Newest</option>
+                            <option value="oldest">Oldest</option>
+                        </select>
+                        <select
+                            value={schemeFilter}
+                            onChange={(e) => setFilter(e.target.value)}
+                            className="w-40 px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
+                        >
+                            <option value="">All</option>
+                            {schemes && schemes.length > 0 && schemes.map((val, index) => (
+                                <option value={val.name} key={val._id}>{val.name}</option>
+                            ))}
 
-                   </select>
-               </div>
+                        </select>
+                    </div>
                     {loading ? (
                         <p>Loading Projects...</p>
                     ) : filteredProjects.length === 0 ? (
@@ -195,17 +191,17 @@ const AdminProposalReview = () => {
                                     <th className="p-2 text-left">Principal Investigator(s)</th>
 
                                     <th className="p-2 text-left">Institute</th>
-                                    
+
                                     <th className="p-2 text-left">Days left</th>
                                 </tr>
                             </thead>
                             <tbody>
                                 {filteredProjects.map((project) => (
                                     <tr key={project.project?._id} className="border-b">
-                                        <td className="p-2 text-sm text-blue-500 hover:underline"  onClick={() => navigate(`/admin/project/${project.project?._id}`)}>{project.project?._id || "N/A"}</td>
-                                        <td className="p-2 text-sm"  onClick={() => navigate(`/admin/project/${project.project?._id}`)}>{project.project?.Title || "N/A"}</td>
-                                        <td className="p-2 text-sm"  onClick={() => navigate(`/admin/project/${project.project?._id}`)}>
-                                        <ul className="list-disc pl-5">
+                                        <td className="p-2 text-sm text-blue-500 hover:underline" onClick={() => navigate(`/admin/project/${project.project?._id}`)}>{project.project?._id || "N/A"}</td>
+                                        <td className="p-2 text-sm" onClick={() => navigate(`/admin/project/${project.project?._id}`)}>{project.project?.Title || "N/A"}</td>
+                                        <td className="p-2 text-sm" onClick={() => navigate(`/admin/project/${project.project?._id}`)}>
+                                            <ul className="list-disc pl-5">
                                                 {project.project.PI?.length > 0
                                                     ? project.project.PI.map((member, index) => (
                                                         <li key={index}>{member}</li>
@@ -213,9 +209,9 @@ const AdminProposalReview = () => {
                                                     : <li>N/A</li>}
                                             </ul>
                                         </td>
-                                       
-                                        <td className="p-2 text-sm"  onClick={() => navigate(`/admin/project/${project.project?._id}`)}>{project.generalInfo?.instituteName || "N/A"}</td>
-                                        <td className="p-2 text-sm"  onClick={() => navigate(`/admin/project/${project.project?._id}`)}>
+
+                                        <td className="p-2 text-sm" onClick={() => navigate(`/admin/project/${project.project?._id}`)}>{project.generalInfo?.instituteName || "N/A"}</td>
+                                        <td className="p-2 text-sm" onClick={() => navigate(`/admin/project/${project.project?._id}`)}>
                                             {(() => {
                                                 const endDate = new Date(project.project?.endDate);
                                                 const timeLeft = endDate - new Date();
@@ -227,7 +223,7 @@ const AdminProposalReview = () => {
                                                 }
                                             })()}
                                         </td>
-                                       
+
                                     </tr>
                                 ))}
                             </tbody>
